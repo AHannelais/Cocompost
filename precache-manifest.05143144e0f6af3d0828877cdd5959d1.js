4f32ed87a09e31f5a32e8218254b45a25001e24f
AHannelais <antoine.hannelais@gmail.com>
2019-08-28 11:16:22 +0200

    Updates

---
+++ precache-manifest.05143144e0f6af3d0828877cdd5959d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c248c9bd31ca3ec64a4d05413fddb5",
+    "url": "/Cocomposte/index.html"
+  },
+  {
+    "revision": "4bdb1132aa6843c58dc9",
+    "url": "/Cocomposte/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9d513d279b2761fef1a7",
+    "url": "/Cocomposte/static/css/main.c23d6ef6.chunk.css"
+  },
+  {
+    "revision": "4bdb1132aa6843c58dc9",
+    "url": "/Cocomposte/static/js/2.2e4edd86.chunk.js"
+  },
+  {
+    "revision": "9d513d279b2761fef1a7",
+    "url": "/Cocomposte/static/js/main.849fedbe.chunk.js"
+  },
+  {
+    "revision": "85b07c756b35bc6e6b7c",
+    "url": "/Cocomposte/static/js/runtime~main.029fd8de.js"
+  },
+  {
+    "revision": "bc7629372f5d12d41649e6a46931b94e",
+    "url": "/Cocomposte/static/media/Amatic-Bold.bc762937.ttf"
+  },
+  {
+    "revision": "8f553edbbfb6b6b0514efa2a70fc953a",
+    "url": "/Cocomposte/static/media/cocoposteTitle1.8f553edb.jpg"
+  },
+  {
+    "revision": "3af8b2ce510fe799ea54ae0592c18860",
+    "url": "/Cocomposte/static/media/home_page_2.3af8b2ce.jpg"
+  },
+  {
+    "revision": "93975aca8bafedb3b09ffae0fe75e6ff",
+    "url": "/Cocomposte/static/media/pouleAndComposte.93975aca.jpg"
+  },
+  {
+    "revision": "af8c3e76ed0526439a5bad74b6ab1971",
+    "url": "/Cocomposte/static/media/poulesPleinAir.af8c3e76.png"
+  },
+  {
+    "revision": "d06f137b0e037cc00f92a0c7edea8153",
+    "url": "/Cocomposte/static/media/poulesVille.d06f137b.jpg"
+  }
+]);
